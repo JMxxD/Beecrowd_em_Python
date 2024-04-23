16e478f734fb1b73eed6b2db1ebe4a7223906adf
João Miguel <167036231+JMxxD@users.noreply.github.com>
2024-04-23 18:31:02 -0300

    1038 - Lanche.py

---
+++ 1038 - Lanche.py
@@ -0,0 +1,22 @@
+A ,B = list(map(int ,input().split()))
+
+if A == 1:
+    V1 = B * 4
+    print('Total: R$ %.2f' %V1)
+
+elif A == 2:
+    V2 = B * 4.5
+    print('Total: R$ %.2f' %V2)
+
+elif A == 3:
+    V3 = B * 5
+    print('Total: R$ %.2f' %V3)
+
+elif A == 4:
+    V4 = B * 2
+    print('Total: R$ %.2f' %V4)
+
+elif A == 5:
+    V5 = B * 1.5
+    print('Total: R$ %.2f' %V5)
+
